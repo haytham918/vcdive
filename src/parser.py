d45bcbbb2f5201346168a77b503e5c7d285d264b
Akshay Tate <54007558+atate75@users.noreply.github.com>
2025-02-09 14:50:59 -0500

    add vpd support

--- src/parser.py
+++ src/parser.py
@@ -2,6 +2,7 @@ import os
 import sys
 import pandas as pd
 import numpy as np
+import json
 
 
 
@@ -13,12 +14,13 @@ def parse_definitions(vcd_file) -> dict:
     # @param line_str: string of the $var section
     # @return line_dict: dictionary of the $var section
     def parse_var(line_str : str) -> dict:
-        line_dict = {}
+        line_dict = dict()
         line_list = line_str.split()
-        line_dict['type'] = line_list[1]
-        line_dict['size'] = line_list[2]
-        line_dict['symbol'] = line_list[3]
-        line_dict['name'] = line_list[4]
+        line_dict[line_list[4]] = dict()
+        line_dict[line_list[4]]['type'] = line_list[1]
+        line_dict[line_list[4]]['size'] = line_list[2]
+        line_dict[line_list[4]]['symbol'] = line_list[3]
+        line_dict[line_list[4]]['name'] = line_list[4]
         return line_dict
 
 
@@ -27,15 +29,15 @@ def parse_definitions(vcd_file) -> dict:
     # @return 
     def parse_scope(vcd_file, scope_name : str) -> dict:
         symbol_table = dict()
-        symbol_table[scope_name] = []
+        symbol_table[scope_name] = dict()
         while True:
             line = vcd_file.readline()
             if line.startswith("$upscope $end"):
                 break
             if line.startswith("$scope"):
-                symbol_table.update(parse_scope(vcd_file, line.split()[2]))
+                symbol_table[scope_name].update(parse_scope(vcd_file, line.split()[2]))
             if line.startswith("$var"):
-                symbol_table[scope_name].append(parse_var(line))
+                symbol_table[scope_name].update(parse_var(line))
         return symbol_table
     
     main_table = dict()
@@ -50,16 +52,14 @@ def parse_definitions(vcd_file) -> dict:
 # Create a symbol table from the main table
 # @param main_table: dictionary of the main table
 # @return symbol_table: dictionary of the symbol table
-def symbol_map(main_table : dict) -> dict:
+def symbol_map(main_table : dict, path = "") -> dict:
     symbol_table = dict()
     for scope in main_table:
         if scope:
-            if type(main_table[scope]) == list:
-                for symbol in main_table[scope]:
-                    symbol_table[symbol['symbol']] = symbol['name']
+            if type(main_table[scope]) == dict and "name" in main_table[scope]:
+                symbol_table[main_table[scope]['symbol']] = path + main_table[scope]['name']
             else:
-                symbol_table.update(symbol_map(main_table[scope]))
-
+                symbol_table.update(symbol_map(main_table[scope], path + f"{scope}."))
     return symbol_table
 
 # Parse a line of data from the VCD file
@@ -77,7 +77,6 @@ def parse_data_line(string : str, symbol_table : dict) -> dict:
         data = string[0]
         symbol = string[1:].strip()
         return {symbol_table[symbol] : data}
-    
     print("[WARNING]: don't know how to parse line: " + string)
     return {}
 
@@ -105,7 +104,34 @@ def parse_data(vcd_file : str, symbol_table : dict) -> dict:
     return data
         
 
+'''
+This parser works for VCD files and VPD that have been converted to VCD
 
+1 ) To generate a VPD:
+
+    Add to testbench code:
+    $vcdplusfile("../cpu.vpd");
+    $vcdpluson;
+
+2) Add alias to bash profile:
+
+    alias vpd2vcd='/opt/caen/synopsys/vcs-2023.12-SP2-1/bin/vpd2vcd +splitpacked'
+            /opt/caen/synopsys/vcs-2023.12-SP2-1/bin/vpd2vcd
+
+3) Add exports:
+
+    export VCS_HOME=/opt/caen/synopsys/vcs-2023.12-SP2-1
+    export PATH=$VCS_HOME/bin:$PATH
+
+4) Convert the VPD to VCD:
+
+    vpd2vcd cpu.vpd cpu2.vcd
+
+5) Run the parser:
+
+    python3 src/parser.py inputs/p3_cpu_vpd.vcd
+
+'''
 if __name__ == "__main__":
     vcd_file = sys.argv[1]
     assert os.path.exists(vcd_file), "File does not exist"
@@ -114,12 +140,14 @@ if __name__ == "__main__":
 
     with open(vcd_file, 'r') as f:
         main_table = parse_definitions(f)
+        with open("symbol_table_debug.json", "w") as fout:
+            json.dump(main_table, fout, indent=4)
         f.readline() # Skip the line after $enddefinitions
         symbol_table = symbol_map(main_table)
         data = parse_data(f, symbol_table)
 
-    df = pd.DataFrame.from_dict(data, orient="index").astype('string')
+    df = pd.DataFrame.from_dict(data, orient="index",dtype=str)
     df.replace("", np.nan, inplace=True)
-    df.fillna(method="ffill", inplace=True) 
-    
-    df.to_csv("output_filled.csv")
+    df.ffill(axis=0, inplace=True)
+    df = df.reset_index().rename(columns={"index": "time"})
+    df.to_csv("output_filled.csv", index=False)
