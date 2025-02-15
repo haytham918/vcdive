50c5e2d6af6d19a90ae803bf6ee40265ca5471a8
haytham918 <yunxuant@umich.edu>
2025-02-15 05:42:34 -0500

    Add method tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,29 +8,58 @@
     -- Textarea of Result 
     -- Reset Button & Upload Buton
 */
-
+"use client";
 import "@/app/home.css";
 import FileUpload from "@/components/FileUpload";
+import MethodTabs from "@/components/MethodTabs";
 import ThemeToggle from "@/components/ThemeToggle";
+import { Upload, FileCloud, Files } from "phosphor-react";
+import { useState, JSX } from "react";
+
+// Methods for sending the file
+export type Method = "drop" | "caen" | "local";
+export interface MethodOption {
+  label: string;
+  value: Method;
+  icon: JSX.Element;
+}
+const available_methods: MethodOption[] = [
+  { label: "Drop", value: "drop", icon: <Upload size={25} weight="bold" /> },
+  { label: "CAEN", value: "caen", icon: <FileCloud size={25} weight="bold" /> },
+  { label: "Local", value: "local", icon: <Files size={25} weight="bold" /> },
+];
+
 const Home = () => {
+  // State variable for the chosen method
+  const [chosen_method, setChosenMethod] = useState<Method>("drop");
+
+  // Button clock handler
+  const chosen_method_handler = (method: Method) => {
+    setChosenMethod(method);
+  };
   return (
     <>
-    <header>
+      <header>
         <ThemeToggle />
-    </header>
-    <main>
-    <div className="home">
-        <div className="title">
-        <h1 className="binary-flip" data-text="VCDive">
-          000000
-        </h1>
-        <h3 className="text-3xl font-bold">Visual Debugger for VCD</h3>
+      </header>
+      <main>
+        <div className="home">
+          <div className="title">
+            <h1 className="binary-flip" data-text="VCDive">
+              000000
+            </h1>
+            <h3 className="text-3xl font-bold">Visual Debugger for VCD</h3>
+          </div>
+          <div className="home-container">
+            <MethodTabs
+              available_methods={available_methods}
+              chosen_method={chosen_method}
+              chosen_method_handler={chosen_method_handler}
+            />
+            <FileUpload />
+          </div>
         </div>
-      <div className="home-container">
-        <FileUpload />
-      </div>
-    </div>
-    </main>
+      </main>
     </>
   );
 };
---
+++ src/components/MethodTabs.tsx
@@ -0,0 +1,28 @@
+"use client";
+import "./MethodTabs.css";
+import { Method, MethodOption } from "@/app/page";
+
+const MethodTabs: React.FC<{
+  available_methods: MethodOption[];
+  chosen_method: Method;
+  chosen_method_handler: (method: Method) => void;
+}> = ({ available_methods, chosen_method, chosen_method_handler }) => {
+  return (
+    <div className="tabs-container">
+      {available_methods.map((method) => (
+        <button
+          className={`btn w-[100px] flex flex-row ${
+            chosen_method === method.value ? "btn-babyblue" : "btn-nonselect"
+          }`}
+          onClick={() => chosen_method_handler(method.value)}
+          key={method.value}
+        >
+          {method.label}
+          {method.icon}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default MethodTabs;
