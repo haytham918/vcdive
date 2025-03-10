389d624e44e2034bb78c48f2fddca3061203c09d
haytham918 <yunxuant@umich.edu>
2025-03-10 00:51:02 -0400

    Fix parse instruction using binary string

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,99 +21,105 @@ import { useState, JSX, useCallback } from "react";
 // Methods for sending the file
 export type Method = "drop" | "caen" | "local";
 export interface MethodOption {
-  label: string;
-  value: Method;
-  icon: JSX.Element;
+    label: string;
+    value: Method;
+    icon: JSX.Element;
 }
 const available_methods: MethodOption[] = [
-  { label: "Drop", value: "drop", icon: <Upload size={25} weight="bold" /> },
-  { label: "CAEN", value: "caen", icon: <FileCloud size={25} weight="bold" /> },
-  { label: "Local", value: "local", icon: <Files size={25} weight="bold" /> },
+    { label: "Drop", value: "drop", icon: <Upload size={25} weight="bold" /> },
+    {
+        label: "CAEN",
+        value: "caen",
+        icon: <FileCloud size={25} weight="bold" />,
+    },
+    { label: "Local", value: "local", icon: <Files size={25} weight="bold" /> },
 ];
 
 const Home = () => {
-  // State variable for the chosen method
-  const [chosen_method, setChosenMethod] = useState<Method>("drop");
+    // State variable for the chosen method
+    const [chosen_method, setChosenMethod] = useState<Method>("drop");
 
-  // File related state variables
-  const [file_name, setFileName] = useState("");
-  const [uploaded_file_name, setUploadedFileName] = useState("");
-  const [is_loading, setLoading] = useState(false);
+    // File related state variables
+    const [file_name, setFileName] = useState("");
+    const [uploaded_file_name, setUploadedFileName] = useState("");
+    const [is_loading, setLoading] = useState(false);
 
-  // Set file name on CAEN or Local
-  const fileNameHandler = useCallback((str: string) => {
-    setFileName(str);
-  }, []);
+    // Set file name on CAEN or Local
+    const fileNameHandler = useCallback((str: string) => {
+        setFileName(str);
+    }, []);
 
-  // Set file name for Upload
-  const uploadedFileNameHandler = useCallback((str: string) => {
-    setUploadedFileName(str);
-  }, []);
+    // Set file name for Upload
+    const uploadedFileNameHandler = useCallback((str: string) => {
+        setUploadedFileName(str);
+    }, []);
 
-  // Set Loadin Icon
-  const loadingHandler = useCallback((loading_val: boolean) => {
-    setLoading(loading_val);
-  }, []);
+    // Set Loadin Icon
+    const loadingHandler = useCallback((loading_val: boolean) => {
+        setLoading(loading_val);
+    }, []);
 
-  // Button clock handler
-  const chosen_method_handler = useCallback((method: Method) => {
-    setChosenMethod(method);
-  }, []);
+    // Button clock handler
+    const chosen_method_handler = useCallback((method: Method) => {
+        setChosenMethod(method);
+    }, []);
 
-  let file_component; // Main layout depending on the file method
-  if (chosen_method === "drop") {
-    file_component = (
-      <FileUpload
-        file_name={uploaded_file_name}
-        is_loading={is_loading}
-        loadingHandler={loadingHandler}
-        fileNameHandler={uploadedFileNameHandler}
-      />
-    );
-  } else if (chosen_method === "caen") {
-    file_component = (
-      <FileCaen
-        file_name={file_name}
-        is_loading={is_loading}
-        loadingHandler={loadingHandler}
-        fileNameHandler={fileNameHandler}
-      />
-    );
-  } else {
-    file_component = (
-      <FileLocal
-        file_name={file_name}
-        is_loading={is_loading}
-        loadingHandler={loadingHandler}
-        fileNameHandler={fileNameHandler}
-      />
-    );
-  }
-  return (
-    <>
-      <header>
-        <ThemeToggle />
-      </header>
-      <main>
-        <div className="home">
-          <div className="title">
-            <h1 className="binary-flip" data-text="VCDive">
-              000000
-            </h1>
-            <h3 className="text-3xl font-bold">Visual Debugger for VCD</h3>
-          </div>
-          <div className="home-container">
-            <MethodTabs
-              available_methods={available_methods}
-              chosen_method={chosen_method}
-              chosen_method_handler={chosen_method_handler}
+    let file_component; // Main layout depending on the file method
+    if (chosen_method === "drop") {
+        file_component = (
+            <FileUpload
+                file_name={uploaded_file_name}
+                is_loading={is_loading}
+                loadingHandler={loadingHandler}
+                fileNameHandler={uploadedFileNameHandler}
             />
-            {file_component}
-          </div>
-        </div>
-      </main>
-    </>
-  );
+        );
+    } else if (chosen_method === "caen") {
+        file_component = (
+            <FileCaen
+                file_name={file_name}
+                is_loading={is_loading}
+                loadingHandler={loadingHandler}
+                fileNameHandler={fileNameHandler}
+            />
+        );
+    } else {
+        file_component = (
+            <FileLocal
+                file_name={file_name}
+                is_loading={is_loading}
+                loadingHandler={loadingHandler}
+                fileNameHandler={fileNameHandler}
+            />
+        );
+    }
+    return (
+        <>
+            <header>
+                <ThemeToggle />
+            </header>
+            <main>
+                <div className="home">
+                    <div className="title">
+                        <h1 className="binary-flip" data-text="VCDive">
+                            000000
+                        </h1>
+                        <h3 className="text-3xl font-bold">
+                            Visual Debugger for VCD
+                        </h3>
+                    </div>
+                    <div className="home-container">
+                        <MethodTabs
+                            available_methods={available_methods}
+                            chosen_method={chosen_method}
+                            chosen_method_handler={chosen_method_handler}
+                        />
+                        {file_component}
+                    </div>
+                </div>
+            </main>
+        </>
+    );
 };
 
 export default Home;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,25 +2,41 @@ import { Instruction } from "./rvcodec.js/Instruction";
 
 export interface DisplayInstruction {
     asm: string;
-    hex: string;
     valid: boolean;
 }
 
-export const parseInstruction = (instruction: number): DisplayInstruction => {
-    const hexadecimal_string = instruction.toString(16).padStart(8, "0");
+// Parse insturction from a binary string
+export const parse_instruction = (instruction: string): DisplayInstruction => {
     try {
-        const inst = new Instruction(hexadecimal_string);
+        const inst = new Instruction(instruction);
+        console.log(inst);
         return {
-            asm: inst.toString(),
-            hex: hexadecimal_string,
+            asm: inst.asm,
             valid: true,
-        }
-    }
-    catch {
+        };
+    } catch {
         return {
-            asm: "Invalid instruction",
-            hex: hexadecimal_string,
+            asm: "Invalid",
             valid: false,
-        }
+        };
     }
-}
+};
+
+// get a cookie value
+export const get_cookie = (name: string) => {
+    const cookie_str = document.cookie;
+    const cookies = cookie_str.split(";").map((c) => c.trim());
+    for (const cookie of cookies) {
+        const [key, value] = cookie.split("=");
+        if (key == name) return decodeURIComponent(value);
+    }
+    return null;
+};
+
+// set a cookie value
+export const set_cookie = (name: string, value: string, days = 365) => {
+    const expires = new Date(Date.now() + days * 864e5).toUTCString();
+    document.cookie = `${name}=${encodeURIComponent(
+        value
+    )}; expires=${expires}; path=/`;
+};
