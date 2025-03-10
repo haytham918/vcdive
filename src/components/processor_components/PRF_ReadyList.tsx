195ef567598df403cf457efe994a6b6f5e2b6440
haytham918 <yunxuant@umich.edu>
2025-03-10 03:10:05 -0400

    update methods and PRF

--- src/components/processor_components/PRF_ReadyList.tsx
+++ src/components/processor_components/PRF_ReadyList.tsx
@@ -2,123 +2,111 @@
 import { NumberSystem } from "@/app/debugger/page";
 import "./Section.css";
 import { MouseEvent, useState } from "react";
+import {
+    process_values,
+    reverse_string,
+    segment_mask_table,
+} from "@/lib/utils";
 
-const SIZE = 16;
-export const segment_mask_table = (size: number, mask: string) => {
-  const segments = [];
-  if (mask) {
-    let remaining_mask = mask;
-    while (remaining_mask.length > 0) {
-      let segment = remaining_mask.slice(-size);
-      remaining_mask = remaining_mask.slice(0, -size);
-
-      if (segment.length < size) {
-        segment.padStart(size - segment.length, "0");
-      }
-      segments.push(segment);
-    }
-  }
-  return segments;
-};
-
-export const process_values = (value: string, number_system: NumberSystem) => {
-  if (value == "" || value == " ") return "x";
-  // Remove leading zeros
-  const trimmedValue = value.replace(/^0+/, "") || "0";
-
-  if (number_system == "0d") {
-    return parseInt(trimmedValue, 16);
-  }
-  return trimmedValue;
-};
+const PRF_SEGMENT_SIZE = 16;
 
 const PRF_ReadyList: React.FC<{
-  selected_number_sys: NumberSystem;
-  ready_list_data: any;
-  prf_data: any;
+    selected_number_sys: NumberSystem;
+    ready_list_data: any;
+    prf_data: any;
 }> = ({ selected_number_sys, ready_list_data, prf_data }) => {
-  const [show_subsection, setShowSubsection] = useState(true);
+    const [show_subsection, setShowSubsection] = useState(true);
 
-  // Open / Close subsection when clicking the header
-  const handleHeaderClick = (event: MouseEvent) => {
-    event.preventDefault();
-    setShowSubsection(!show_subsection);
-  };
+    // Open / Close subsection when clicking the header
+    const handleHeaderClick = (event: MouseEvent) => {
+        event.preventDefault();
+        setShowSubsection(!show_subsection);
+    };
 
-  // Extract the ready_list_data
-  const ready_state_mask: string =
-    ready_list_data["READY_LIST.ready_state_mask"];
-  const ready_segments = ready_state_mask
-    ? segment_mask_table(SIZE, ready_state_mask)
-    : ["0".repeat(SIZE), "0".repeat(SIZE), "0".repeat(SIZE), "0".repeat(SIZE)];
-  // console.log(ready_segments);
+    // Extract the ready_list_data and Reverse it
+    const ready_state_mask: string = reverse_string(
+        ready_list_data["READY_LIST.ready_state_mask"]
+    );
+    // Segment the mask
+    const ready_segments =
+        ready_state_mask != ""
+            ? segment_mask_table(PRF_SEGMENT_SIZE, ready_state_mask)
+            : [
+                  "0".repeat(PRF_SEGMENT_SIZE),
+                  "0".repeat(PRF_SEGMENT_SIZE),
+                  "0".repeat(PRF_SEGMENT_SIZE),
+                  "0".repeat(PRF_SEGMENT_SIZE),
+              ];
+    // console.log(ready_segments);
 
-  const prf_vals: (string | number)[] = [0];
-  if (ready_state_mask) {
-    for (let i = 1; i < ready_state_mask.length; i++) {
-      const processed_value = process_values(
-        prf_data[`REGFILE.regfile_mem.mem_data[${i}]`],
-        selected_number_sys
-      );
-      prf_vals.push(processed_value);
+    const prf_vals: (string | number)[] = [0];
+    if (ready_state_mask) {
+        for (let i = 1; i < ready_state_mask.length; i++) {
+            const processed_value = process_values(
+                prf_data[`REGFILE.regfile_mem.mem_data[${i}]`],
+                selected_number_sys
+            );
+            prf_vals.push(processed_value);
+        }
     }
-  }
 
-  const tables = ready_segments.map((ready_segment, segment_index) => (
-    <table key={segment_index}>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Value</th>
-          <th>Ready</th>
-        </tr>
-      </thead>
+    const tables = ready_segments.map((ready_segment, segment_index) => (
+        <table key={segment_index}>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Value</th>
+                    <th>Ready</th>
+                </tr>
+            </thead>
 
-      {/* Actual Register Values */}
-      <tbody>
-        {Array.from({ length: SIZE }, (_, i) => {
-          const bit_index = ready_segment.length - i - 1;
-          const ready = ready_segment[bit_index] == "1" ? "Y" : "N";
-          const reg_index = segment_index * SIZE + i;
-          const value = reg_index < prf_vals.length ? prf_vals[reg_index] : "x";
-          const is_emerald = ready === "Y" && value !== "x"; // Green: Ready and val not "x"
-          const is_red = value === "x"; // Red: value is "x"
-          // Determine cell color
-          let color;
-          if (is_emerald) color = "emerald";
-          else if (is_red) color = "red";
-          else color = "";
-          return (
-            <tr key={i}>
-              <td className={color}>{reg_index}</td>
-              <td className={color}>{value}</td>
-              <td className={color}>{ready}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  ));
+            {/* Actual Register Values */}
+            <tbody>
+                {Array.from({ length: PRF_SEGMENT_SIZE }, (_, i) => {
+                    const ready = ready_segment[i] == "1" ? "Y" : "N";
+                    const reg_index = segment_index * PRF_SEGMENT_SIZE + i;
+                    const value =
+                        reg_index < prf_vals.length ? prf_vals[reg_index] : "x";
+                    const is_emerald = ready === "Y" && value !== "x"; // Green: Ready and val not "x"
+                    const is_red = value === "x"; // Red: value is "x"
+                    // Determine cell color
+                    let color;
+                    if (is_emerald) color = "emerald";
+                    else if (is_red) color = "red";
+                    else color = "";
+                    return (
+                        <tr key={i}>
+                            <td className={color}>{reg_index}</td>
+                            <td className={color}>{value}</td>
+                            <td className={color}>{ready}</td>
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
+    ));
 
-  const subsection_comp = show_subsection ? (
-    <div className="section sub-section">
-      <h2 className="subsection-header">Status</h2>
+    const subsection_comp = show_subsection ? (
+        <div className="section sub-section">
+            <h2 className="subsection-header">Status</h2>
 
-      <div className="flex flex-row gap-x-1">
-        {/* Creating Segment Tables */}
-        {tables}
-      </div>
-    </div>
-  ) : null;
+            <div className="flex flex-row gap-x-1">
+                {/* Creating Segment Tables */}
+                {tables}
+            </div>
+        </div>
+    ) : null;
 
-  return (
-    <div className="section main-section">
-      <a onClick={handleHeaderClick}>
-        <h1 className="mainsection-header">Physical Registers + Ready List</h1>
-      </a>
-      {subsection_comp}
-    </div>
-  );
+    return (
+        <div className="section main-section">
+            <a onClick={handleHeaderClick}>
+                <h1 className="mainsection-header">
+                    Physical Registers + Ready List
+                </h1>
+            </a>
+            {subsection_comp}
+        </div>
+    );
 };
 
 export default PRF_ReadyList;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,34 @@
+import { NumberSystem } from "@/app/debugger/page";
 import { Instruction } from "./rvcodec.js/Instruction";
 
+/*
+        GUI Methods
+*/
+// ---------------------------------------------------------------
+// get a cookie value
+export const get_cookie = (name: string) => {
+    const cookie_str = document.cookie;
+    const cookies = cookie_str.split(";").map((c) => c.trim());
+    for (const cookie of cookies) {
+        const [key, value] = cookie.split("=");
+        if (key == name) return decodeURIComponent(value);
+    }
+    return null;
+};
+
+// set a cookie value
+export const set_cookie = (name: string, value: string, days = 365) => {
+    const expires = new Date(Date.now() + days * 864e5).toUTCString();
+    document.cookie = `${name}=${encodeURIComponent(
+        value
+    )}; expires=${expires}; path=/`;
+};
+/* ---------------------------------------------------------------*/
+
+/*
+        Debugger Methods
+*/
+// ---------------------------------------------------------------
 export interface DisplayInstruction {
     asm: string;
     valid: boolean;
@@ -22,21 +51,53 @@ export const parse_instruction = (instruction: string): DisplayInstruction => {
     }
 };
 
-// get a cookie value
-export const get_cookie = (name: string) => {
-    const cookie_str = document.cookie;
-    const cookies = cookie_str.split(";").map((c) => c.trim());
-    for (const cookie of cookies) {
-        const [key, value] = cookie.split("=");
-        if (key == name) return decodeURIComponent(value);
-    }
-    return null;
+// Convert register number to decimal
+export const convert_reg_hex_to_dec = (reg_hex: string) => {
+    const reg_dec = parseInt(reg_hex, 16);
+    return reg_dec;
 };
 
-// set a cookie value
-export const set_cookie = (name: string, value: string, days = 365) => {
-    const expires = new Date(Date.now() + days * 864e5).toUTCString();
-    document.cookie = `${name}=${encodeURIComponent(
-        value
-    )}; expires=${expires}; path=/`;
+// Process value based on the selected number system
+export const process_values = (value: string, number_system: NumberSystem) => {
+    if (value == "" || value == " ") return "x";
+    // Remove leading zeros
+    const trimmedValue = value.replace(/^0+/, "") || "0";
+
+    if (number_system == "0d") {
+        return parseInt(trimmedValue, 16);
+    }
+    return trimmedValue;
 };
+
+// Reverse string method
+export const reverse_string = (str: string) => {
+    if (str) return str.split("").reverse().join("");
+    return "";
+};
+
+// Create mask segments based on "reversed" mask string
+// If original string is "0001", you need to pass it as "1000" here
+// Result:
+/*
+    Size: 2, Mask: 1011
+    ==> [10, 11]
+*/
+export const segment_mask_table = (size: number, mask: string) => {
+    const segments = [];
+    if (mask) {
+        let remaining_mask = mask;
+        while (remaining_mask.length > 0) {
+            let segment = remaining_mask.slice(0, size);
+            remaining_mask = remaining_mask.slice(size);
+
+            if (segment.length < size) {
+                // We would pad 0 to the end instead
+                segment.padEnd(size - segment.length, "0");
+            }
+            segments.push(segment);
+        }
+    }
+    return segments;
+};
+
+// -----------------------------------------------------------------
