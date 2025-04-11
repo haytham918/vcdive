9f74fb3a1e52cba81ff29383991c60d0359d07f2
haytham918 <yunxuant@umich.edu>
2025-04-10 21:30:21 -0400

    Add ignore lint for build

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  async rewrites() {
-    return [
-      {
-        source: "/backend/:path*",
-        destination: "http://127.0.0.1:5000/backend/:path*",
-      },
-    ];
-  },
+    /* config options here */
+    async rewrites() {
+        return [
+            {
+                source: "/backend/:path*",
+                destination: "http://127.0.0.1:5000/backend/:path*",
+            },
+        ];
+    },
+    eslint: {
+        // Warning: This will allow production builds to complete even if there are lint errors.
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
